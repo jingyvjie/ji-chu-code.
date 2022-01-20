880951f88b5f72363c9fe976aff98e8d93b78bc6
Administrator <596897626@qq.com>
2022-01-20 17:13:27 +0800

    python接口

---
+++ 07-对象/15-导包.py
@@ -0,0 +1,7 @@
+from Cat import cat as cat22
+
+cat22().eat("鱼")
+
+import Cat
+
+Cat.cat().eat("鱼")
---
+++ 07-对象/Cat.py
@@ -0,0 +1,13 @@
+class cat:
+    def eat(self,food):
+        print("猫咪等着吃%s" % food)
+
+    def drink(self):
+        pass
+# class dog:
+#     def eat(self,food):
+#         print("狗爱吃%s"%food)
+# obj=cat()
+# obj.eat("肉")
+
+
